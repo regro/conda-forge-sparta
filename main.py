f6e2f100019c31790f4a3c1c21410f299418f77e
Matthew R Becker <becker.mr@gmail.com>
2020-08-14 17:22:00 -0500

    no more index pages

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ LINKS = requests.get(
 
 @setInterval(300)  # every 5 minutes
 def _update_links():
-    print("RELOADING LINKS")
+    print("************* RELOADING LINKS *************")
     global LINKS
     new_links = requests.get(
         "https://github.com/regro/repodata/releases/latest/download/links.json"
@@ -44,9 +44,102 @@ _stop_update_links = _update_links()
 app = FastAPI()
 
 
+################################################################################
+# labels
+################################################################################
+
+@app.get("/label/{label}")
+async def root_label(label):
+    return {"message": "this is the index!"}
+    # return RedirectResponse(
+    #     f"https://regro.github.io/repodata/label/{label}/index.html"
+    # )
+
+
+@app.get("/label/{label}/channeldata.json")
+async def channeldata_label(label):
+    return RedirectResponse(
+        "https://github.com/regro/repodata/releases/latest/download/"
+        f"channeldata_{label}.json"
+    )
+
+
+@app.get("/label/{label}/{subdir}/")
+async def subdir_root_label(label, subdir):
+    return {"message": "this is the index!"}
+    # return RedirectResponse(
+    #     f"https://regro.github.io/repodata/label/{label}/{subdir}/index.html"
+    # )
+
+
+@app.get("/label/{label}/{subdir}/repodata.json")
+async def subdir_repodatadata_label(label, subdir):
+    return RedirectResponse(
+        "https://github.com/regro/repodata/releases/latest/download/"
+        f"repodata_{subdir}_{label}.json"
+    )
+
+
+@app.get("/label/{label}/{subdir}/repodata.json.bz2")
+async def subdir_repodatadatabz2_label(label, subdir):
+    return RedirectResponse(
+        "https://github.com/regro/repodata/releases/latest/download/"
+        f"repodata_{subdir}_{label}.json.bz2"
+    )
+
+
+@app.get("/label/{label}/{subdir}/repodata_from_packages.json")
+async def subdir_repodatadata_pkgs_label(label, subdir):
+    return RedirectResponse(
+        "https://github.com/regro/repodata/releases/latest/download/"
+        f"repodata_{subdir}_{label}.json"
+    )
+
+
+@app.get("/label/{label}/{subdir}/repodata_from_packages.json.bz2")
+async def subdir_repodatadatabz2_pkgs_label(label, subdir):
+    return RedirectResponse(
+        "https://github.com/regro/repodata/releases/latest/download/"
+        f"repodata_{subdir}_{label}.json.bz2"
+    )
+
+
+@app.get("/label/{label}/{subdir}/current_repodata.json")
+async def subdir_repodatadata_curr_label(label, subdir):
+    return RedirectResponse(
+        "https://github.com/regro/repodata/releases/latest/download/"
+        f"repodata_{subdir}_{label}.json"
+    )
+
+
+@app.get("/label/{label}/{subdir}/current_repodata.json.bz2")
+async def subdir_repodatadatabz2_curr_label(label, subdir):
+    return RedirectResponse(
+        "https://github.com/regro/repodata/releases/latest/download/"
+        f"repodata_{subdir}_{label}.json.bz2"
+    )
+
+
+@app.get("/label/{label}/{subdir}/{pkg}")
+async def subdir_pkg_label(label, subdir, pkg):
+    subdir_pkg = os.path.join(subdir, pkg)
+    url = LINKS[label].get(subdir_pkg, None)
+    if url is None:
+        raise HTTPException(
+            status_code=404, detail=f"label/{label}/{subdir_pkg} not found!"
+        )
+    return RedirectResponse(url)
+
+
+################################################################################
+# main
+################################################################################
+
+
 @app.get("/")
 async def root():
-    return RedirectResponse("https://regro.github.io/repodata/index.html")
+    return {"message": "this is the index!"}
+    # return RedirectResponse("https://regro.github.io/repodata/label/main/index.html")
 
 
 @app.get("/channeldata.json")
@@ -59,7 +152,10 @@ async def channeldata():
 
 @app.get("/{subdir}/")
 async def subdir_root(subdir):
-    return RedirectResponse(f"https://regro.github.io/repodata/{subdir}/index.html")
+    return {"message": "this is the index!"}
+    # return RedirectResponse(
+    #     f"https://regro.github.io/repodata/label/main/{subdir}/index.html"
+    # )
 
 
 @app.get("/{subdir}/repodata.json")
